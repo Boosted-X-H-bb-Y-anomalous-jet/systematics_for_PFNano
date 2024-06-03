0f46a024c9083f16502e2c22b7057d40318d6ea6
Matej Roguljic <matej.roguljic@gmail.com>
2024-06-03 03:52:59 -0500

    small fix

--- exe_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while read -r line
-do
-    echo python addSys.py ${line}
-    python addSys.py ${line}
-done < test.txt
-
-#xrdcp -f rootfiles/THselection_*.root root://cmseos.fnal.gov//store/user/roguljic/2017/selection/
--- timber_tar.sh
+++ timber_tar.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd ..
 tar -cf timber_tar.tgz TIMBER
 xrdcp -f timber_tar.tgz root://cmseos.fnal.gov//store/user/roguljic/timber_tar.tgz
